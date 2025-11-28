d2f5d997a86b2e3a55cdc93f06c2c90b459c46e2
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-28 11:44:28 +0100

    011 Creamos la clase Order

---
+++ app/src/main/java/Order.java
@@ -0,0 +1,44 @@
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Order {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public int productID;
+
+    public double totalPrice;
+
+    public Order() {}
+
+    public Order(double totalPrice, int productID, int id) {
+        this.totalPrice = totalPrice;
+        this.productID = productID;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
