e0fbe730109de8369a83340ec1697a43063ab52e
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-28 11:34:11 +0100

    009 Creamos la clase mesa

---
+++ app/src/main/java/Table.java
@@ -0,0 +1,38 @@
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Table {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public int tableNumber;
+
+    public int space;
+
+    public Table() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getSpace() {
+        return space;
+    }
+
+    public void setSpace(int space) {
+        this.space = space;
+    }
+}
