30fdcb94732eeccc1ea1799a2e91bc07a9d01110
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-28 11:23:15 +0100

    008 Editamos la clase productos

--- app/src/main/java/Products.java
+++ app/src/main/java/Products.java
@@ -1,2 +1,44 @@
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Products {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String productName;
+
+    public int stock;
+
+    public Products() {}
+
+    public Products(int stock, String productName, int id) {
+        this.stock = stock;
+        this.productName = productName;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
