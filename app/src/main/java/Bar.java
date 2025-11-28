6e621c612293ecb844bedd145ed3b4bd5522b050
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-11-28 11:27:29 +0100

    009 Creamos la clase bar

--- app/src/main/java/Bar.java
+++ app/src/main/java/Bar.java
@@ -10,4 +10,35 @@ public class Bar {
 
     public String email;
 
+    public Bar() {}
+
+    public Bar(int id, String barName, String email) {
+        this.id = id;
+        this.barName = barName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBarName() {
+        return barName;
+    }
+
+    public void setBarName(String barName) {
+        this.barName = barName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
