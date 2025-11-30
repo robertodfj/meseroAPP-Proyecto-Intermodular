4fe97650a80c1800e5ede02144f1ecaf71e8c1cf
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-01 00:01:47 +0100

    027 Creamos la logica de mesa/table

--- app/src/main/java/data/dao/TableDAO.java
+++ app/src/main/java/data/dao/TableDAO.java
@@ -15,6 +15,45 @@ public interface TableDAO {
     @Query("SELECT * FROM `Table` WHERE id = :id")
     Table getById(int id);
 
+    @Query("SELECT * FROM `Table` WHERE tableNumber = :tableNumber")
+    Table getByTableNumber(int tableNumber);
+
     @Delete
     void delete(Table table);
+
+    @Query("UPDATE `Table` SET tableNumber = :newTableNumber WHERE tableNumber = :tableNumber")
+    void updateTableNumber(int tableNumber, int newTableNumber);
+
+    public default boolean addTable(Table table){
+        Table existTable = getByTableNumber(table.getTableNumber());
+        if (existTable == null) {
+            insert(table);
+            System.out.println("Mesa numero " +table.getTableNumber()+ " añadida con exito");
+            return true;
+        }
+        System.out.println("Ese numero de mesa ya esta en uso");
+        return false;
+    }
+
+    public default boolean deleteTable(int tableNumber){
+        Table table = getByTableNumber(tableNumber);
+        if (table != null){
+            delete(table);
+            System.out.println("Mesa numero " +tableNumber+ " eliminada con exito");
+            return true;
+        }
+        System.out.println("Fallo al eliminar la mesa " +tableNumber+ " comprueba que esta existe");
+        return false;
+    }
+
+    public default boolean editTable(int tableNumber, int newTableNumber){
+        Table existTable = getByTableNumber(tableNumber);
+        if (existTable != null){
+            updateTableNumber(tableNumber, newTableNumber);
+            System.out.println("Mesa numero: " +tableNumber+ " renombrada a " +newTableNumber+ " con exito");
+            return true;
+        }
+        System.out.println("La mesa numero " +tableNumber+ " no existe");
+        return false;
+    }
 }
