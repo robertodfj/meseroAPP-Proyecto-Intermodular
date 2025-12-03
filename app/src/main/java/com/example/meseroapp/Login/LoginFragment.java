a340039086dca4d751372e0f530aa8e0741e827f
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-03 03:41:51 +0100

    042 Creamos el register fragment y el cambio entre fragmentos con FragmentManager funciona

--- app/src/main/java/com/example/meseroapp/Login/LoginFragment.java
+++ app/src/main/java/com/example/meseroapp/Login/LoginFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.meseroapp.R;
+import com.example.meseroapp.RegisterFragment;
 
 public class LoginFragment extends Fragment {
 
@@ -30,6 +32,17 @@ public class LoginFragment extends Fragment {
 
         // Aquí conectarás botones, listeners y lógica del login.
         // Ejemplo:
-        // Button btnLogin = view.findViewById(R.id.btnLogin);
+        Button btn = view.findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegisterFragment registerFragment = new RegisterFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, registerFragment) // fragment_container es el FrameLayout del Activity
+                        .addToBackStack(null) // Permite volver al login con el botón atrás
+                        .commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/meseroapp/RegisterFragment.java
@@ -0,0 +1,34 @@
+package com.example.meseroapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Aquí conectarás botones, listeners y lógica del login.
+        // Ejemplo:
+        // Button btnLogin = view.findViewById(R.id.btnLogin);
+    }
+}
