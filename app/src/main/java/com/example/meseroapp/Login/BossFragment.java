691873c670117529b2ff0b1acfb0960a2aa60183
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-04 02:21:03 +0100

    043 Creamos el fragmento para crear Bares (BOSS FRAGMENT)

---
+++ app/src/main/java/com/example/meseroapp/Login/BossFragment.java
@@ -0,0 +1,37 @@
+package com.example.meseroapp.Login;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.meseroapp.R;
+
+public class BossFragment extends Fragment {
+
+    public BossFragment() {}
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_boss, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Aquí conectarás botones, listeners y lógica del login.
+        // Ejemplo:
+        //Button btn = view.findViewById(R.id.button);
+
+    }
+}
