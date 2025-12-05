05982488162481261595bd81bd8ccedb1844fe8c
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-05 03:15:39 +0100

    050 Creamos la parte estetica del login y conexion con los fragmentos

--- app/src/main/java/com/example/meseroapp/Login/LoginFragment.java
+++ app/src/main/java/com/example/meseroapp/Login/LoginFragment.java
@@ -1,20 +1,33 @@
 package com.example.meseroapp.Login;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.meseroapp.R;
 
 public class LoginFragment extends Fragment {
 
-    public LoginFragment() {
+    // Elementos del layout
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvRegister, tvBossRegister;
 
+    public LoginFragment() {
     }
 
     @Override
@@ -29,29 +42,31 @@ public class LoginFragment extends Fragment {
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Aquí conectarás botones, listeners y lógica del login.
-        // Ejemplo:
-        Button btn = view.findViewById(R.id.button);
-        Button btn2 = view.findViewById(R.id.buttonBoss);
+        // Conexión con los elementos
+        etEmail = view.findViewById(R.id.etEmail);
+        etPassword = view.findViewById(R.id.etPassword);
+        btnLogin = view.findViewById(R.id.btnLogin);
+        tvRegister = view.findViewById(R.id.tvRegister);
+        tvBossRegister = view.findViewById(R.id.tvBossRegister);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RegisterFragment registerFragment = new RegisterFragment();
                 getParentFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, registerFragment) // fragment_container es el FrameLayout del Activity
-                        .addToBackStack(null) // Permite volver al login con el botón atrás
+                        .replace(R.id.fragment_container, registerFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver al login con el botón atrás
                         .commit();
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener() {
+        tvBossRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BossFragment bossFragment = new BossFragment();
                 getParentFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, bossFragment) // fragment_container es el FrameLayout del Activity
-                        .addToBackStack(null) // Permite volver al login con el botón atrás
+                        .replace(R.id.fragment_container, bossFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver al login con el botón atrás
                         .commit();
             }
         });
