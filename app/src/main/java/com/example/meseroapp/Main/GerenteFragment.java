a1a80941243909b235880985cc6494e4ff2b62de
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-09 18:45:38 +0100

    068 Creada la seccion de gerente para poder realziar las acciones de jefe

--- app/src/main/java/com/example/meseroapp/Main/GerenteFragment.java
+++ app/src/main/java/com/example/meseroapp/Main/GerenteFragment.java
@@ -1,15 +1,16 @@
 package com.example.meseroapp.Main;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.example.meseroapp.Login.BossFragment;
 import com.example.meseroapp.R;
 
 public class GerenteFragment extends Fragment {
@@ -28,8 +29,58 @@ public class GerenteFragment extends Fragment {
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Aquí conectarás botones, listeners y lógica del login.
-        // Ejemplo:
-        //Button btn = view.findViewById(R.id.button);
+        Button btnVerAlmacen = view.findViewById(R.id.btnVerAlmacen);
+        Button btnVerEmpleados = view.findViewById(R.id.btnVerEmpleados);
+        Button btnVerCocina = view.findViewById(R.id.btnVerCocina);
+        Button btnVerMesas = view.findViewById(R.id.btnVerMesas);
+
+        // TODO:
+        btnVerAlmacen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlmacenFragment almacenFragment = new AlmacenFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, almacenFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver con el botón atrás
+                        .commit();
+            }
+        });
+
+        // TODO:
+        btnVerEmpleados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EmpleadosFragment empleadosFragment = new EmpleadosFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, empleadosFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver con el botón atrás
+                        .commit();
+            }
+        });
+
+        btnVerCocina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CocinaFragment cocinaFragment = new CocinaFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, cocinaFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver con el botón atrás
+                        .commit();
+            }
+        });
+
+        btnVerMesas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CamareroFragment camareroFragment = new CamareroFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, camareroFragment) // frameLayout del Activity
+                        .addToBackStack(null) // permite volver con el botón atrás
+                        .commit();
+            }
+        });
+
+
+
     }
 }
