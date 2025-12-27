6dc73cdb01b59a9bc2654b983fccb7ff6bb39949
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-12-27 17:34:08 +0100

    162 Añadimos que si no se guarda el usuario logeado se vuelve al login

--- app/src/main/java/com/example/meseroapp/Main/MainActivity.java
+++ app/src/main/java/com/example/meseroapp/Main/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.meseroapp.Main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.meseroapp.Login.LoginRegisterActivity;
 import com.example.meseroapp.Main.Boss.BossFragment;
 import com.example.meseroapp.R;
 import com.example.meseroapp.utils.SessionManager;
@@ -13,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!SessionManager.getInstance(this).isLogged()) {
+            // Redirigir al login si no hay sesión
+            startActivity(new Intent(this, LoginRegisterActivity.class));
+            finish();
+            return;
+        }
+
         setContentView(R.layout.activity_main);
+
         String role = SessionManager.getInstance(this).getUserRole();
 
         switch (role) {
